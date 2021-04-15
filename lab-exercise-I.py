d8051d9249c9169cc3abfd7b3b774246c4c0c129
David Ikwuegbu <david.ikwuegbu@pau.edu.ng>
2021-04-15 17:51:13 +0200

    My Instructions

---
+++ lab-exercise-I.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # David IKWUEGBU
+# # 20120612107
+# # david.ikwuegbu@pau.edu.ng
+
+# # Exercise 1
+# # A pyhton program to get the difference between a given number and 17, if the number is greater than 17 return double the absolute difference.
+# ![image.png](attachment:image.png)
+
+# In[11]:
+
+
+x = 50
+y = 30
+z = x - y
+if x > 17:
+    print(abs(z*2))
+else:
+    print(z)
+
+
+# # Exercise 2
+# # A Python program to calculate the sum of three given numbers, if the values are equal then return thrice of their sum.
+# ![image.png](attachment:image.png)
+
+# In[23]:
+
+
+w = 10
+x = 10
+y = 10
+if w == x == y:
+    print((w + x + y)*3)
+else:
+    print(w + x + y)
+
+
+# # Exercise 3
+# # A Python program which will return true if the two given integer values are equal or their sum or difference is 5.
+# ![image.png](attachment:image.png)
+
+# In[42]:
+
+
+a = 8
+b = 3
+c = a + b
+d = a - b
+if a == b:
+    print(True)
+if c == 5:
+    print(True)
+if d == 5:
+    print (True)
+
+
+# # Exercise 4
+# # A Python program to sort three integers without using conditional statements and loops.
+# ![image.png](attachment:image.png)
+
+# In[ ]:
+
+
+
+
+
+# # Exercise 5
+# # A Python function that takes a positive integer and returns the sum of the cube of all the positive integers smaller than the specified number.
+# ![image.png](attachment:image.png)
+
+# In[50]:
+
+
+print (range[6])
+
+
+# In[ ]:
+
+
+
+
