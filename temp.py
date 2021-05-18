6233d23bf2e8fe87f0886d0d93ad79855a8e5845
David Ikwuegbu <david.ikwuegbu@pau.edu.ng>
2021-05-18 14:02:46 +0100

    Dr. Kazeem's first class

---
+++ temp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+class Coffee:
+    
+    def __init__(self, themilk, thesugar, thecoffeemate):
+        self.milk  = themilk
+        self.sugar = thesugar
+        self.goffeemate = thecoffeemate
+        print(f"you now have your coffee with {self.mix} milk, {self.sugar} sugar and {self.sugar} coffeemate")
+        
+        
+    my
